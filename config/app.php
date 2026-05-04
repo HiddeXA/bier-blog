57b535c9eb08c27c475c3d8df30f23a6b551fa53
HiddeXA <hiddealberts@gmail.com>
2026-05-04 21:53:04 +0200

    feat: add cron token configuration and route for schedule execution

--- config/app.php
+++ config/app.php
@@ -99,6 +99,8 @@ return [
 
     'key' => env('APP_KEY'),
 
+    'cron_token' => env('CRON_TOKEN'),
+
     'previous_keys' => [
         ...array_filter(
             explode(',', (string) env('APP_PREVIOUS_KEYS', ''))
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('/cron/{token}', function (string $token) {
+    if ($token !== config('app.cron_token')) {
+        abort(403);
+    }
+    Artisan::call('schedule:run');
+    return response()->noContent();
+});
